908a173413542ddab1a87b9d66dca0c97a7af9b6
Hugo Hernani <hhernanni@gmail.com>
2023-03-19 13:29:56 -0300

    Add initial app setup

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module BatchUserCreation
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    config.generators do |generator|
+      generator.test_framework :rspec
+      generator.helper false
+      generator.assets false
+      generator.view_specs false
+    end
+  end
+end

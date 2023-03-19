3a08b70534a6450b5af07db602a808af02653023
Hugo Hernani <hhernanni@gmail.com>
2023-03-19 14:15:17 -0300

    Fix factory bot setup by allowing rspec to load its syntax

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,5 @@
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end

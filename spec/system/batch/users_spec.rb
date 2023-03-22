c69ca505b66b195a93a9916413d7e7ec4c28f849
Hugo Hernani <hhernanni@gmail.com>
2023-03-21 22:44:02 -0300

    Merge user batch flow with stream response in it
    commit ff3cf9200d031e558a5cb6eeb3c48e7b765597bc
    Author: Hugo Hernani <hhernanni@gmail.com>
    Date:   Tue Mar 21 22:04:44 2023 -0300
    
        Build the happy path batch flow with stream response
    
    commit cfdefcb605ac86eccc046b46b6acd125d45a0d01
    Author: Hugo Hernani <hhernanni@gmail.com>
    Date:   Tue Mar 21 00:17:08 2023 -0300
    
        Add job basic definition for User Batch

--- spec/system/batch/users_spec.rb
+++ spec/system/batch/users_spec.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require 'rails_helper'
 
-RSpec.describe 'Batch::Users' do
+RSpec.describe 'Batch::Users', inline_sidekiq: true do
   describe 'uploading a batch users csv file' do
     context 'when all users are valid' do
       let(:valid_csv_file_path) { Rails.root.join('spec', 'fixtures', 'batch', 'users', 'valid.csv') }
@@ -12,7 +12,7 @@ RSpec.describe 'Batch::Users' do
         attach_file('file_handler[csv]', valid_csv_file_path)
         submit_form
 
-        within '#users_listing' do
+        within '#user_responses' do
           users = peek_users(valid_csv_file_path, 3)
 
           expect(page).to have_content(users[0])
@@ -22,28 +22,37 @@ RSpec.describe 'Batch::Users' do
       end
     end
 
-    context 'when there are invalid users' do
+    context 'when there are invalid users', headless: false do
       let(:invalid_csv_file_path) { Rails.root.join('spec', 'fixtures', 'batch', 'users', 'invalid.csv') }
 
       it 'creates each user in it' do
         visit root_path
 
-        attach_file('users_batch', invalid_csv_file_path)
+        attach_file('file_handler[csv]', invalid_csv_file_path)
+        submit_form
 
-        within '#users_listing' do
+        within '#user_responses' do
           users = peek_users(invalid_csv_file_path, 3)
 
-          # TODO: Filter element to confirm it's invalid
-          expect(page).to have_content(users[0])
-          expect(page).to have_content(users[1])
-          expect(page).to have_content(users[2])
+          apply_expectation_for_invalid_user(users[0], 1)
+          apply_expectation_for_invalid_user(users[1], 2)
+          apply_expectation_for_invalid_user(users[2], 3)
         end
       end
+    end
+
+    def submit_form
+      find('input[name="commit"]').click
     end
 
     def peek_users(file_path, quantity)
       csv = CSV.foreach(file_path, headers: true).take(quantity)
       csv.pluck('name')
+    end
+
+    def apply_expectation_for_invalid_user(user, mocked_row_number)
+      expect(page).to have_content("(#{mocked_row_number} - INVALID)")
+      expect(page).to have_content(user)
     end
   end
 end
